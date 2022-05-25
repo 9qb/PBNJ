91624541e926007a87ace1826e7e71a9862764a3
bli31 <bli31@stuy.edu>
2022-05-25 13:16:49 -0400

    init v2

---
+++ v2/Map.java
@@ -0,0 +1,121 @@
+public class Map{
+
+    String hero = "X";
+    int heroX = 1;
+    int heroY = 0;
+
+    private static void clear()
+    {
+      System.out.println("\033[2J");
+      System.out.println("\033[" + 1 + ";" + 1 + "H");
+    }
+
+    private Maze maze;
+
+    // will determine which frame (aka Maze) will be shown on screen
+    private Maze currentFrame;
+
+    public Map(){
+        MazeGenerator troll = new MazeGenerator(18, 32);
+        maze = new Maze(troll.getGeneratedMaze());
+        currentFrame = maze;
+        currentFrame.setPos(heroX, heroY, hero);
+    }
+
+    /*
+    Methods to change Frames
+    */
+    public void changeNorth(){
+        if(currentFrame.getNorth() != null){
+            currentFrame = currentFrame.getNorth();
+        }
+    }
+    public void changeEast(){
+        if(currentFrame.getEast() != null){
+            currentFrame = currentFrame.getEast();
+        }
+    }
+    public void changeSouth(){
+        if(currentFrame.getSouth() != null){
+            currentFrame = currentFrame.getSouth();
+        }
+    }
+    public void changeWest(){
+        if(currentFrame.getWest() != null){
+            currentFrame = currentFrame.getWest();
+        }
+    }
+
+    public void setCurrentFrame(Maze currentMaze){
+        this.currentFrame = currentMaze;
+    }
+
+    public String toString(){
+        clear();
+        return currentFrame.toString();
+    }
+
+    public void moveUp(){
+        if(currentFrame.getPos(heroX-1, heroY).equals(" ")){
+            currentFrame.setPos(heroX, heroY,".");
+            heroX -= 1;
+            currentFrame.setPos(heroX, heroY, hero);
+        }
+    }
+    public void moveRight(){
+        if(currentFrame.getPos(heroX, heroY+1).equals(" ")){
+            currentFrame.setPos(heroX, heroY,".");
+            heroY += 1;
+            currentFrame.setPos(heroX, heroY, hero);
+        }
+    }
+    public void moveDown(){
+        if(currentFrame.getPos(heroX+1, heroY).equals(" ")){
+            currentFrame.setPos(heroX, heroY,".");
+            heroX += 1;
+            currentFrame.setPos(heroX, heroY, hero);
+        }
+    }
+    public void moveLeft(){
+        if(currentFrame.getPos(heroX, heroY-1).equals(" ")){
+            currentFrame.setPos(heroX, heroY,".");
+            heroY -= 1;
+            currentFrame.setPos(heroX, heroY, hero);
+        }
+    }
+
+    public static void main(String[] args) {
+        Map test = new Map();
+        System.out.println(test);
+        System.out.println("______________");
+
+        test.changeEast();
+        System.out.println(test);
+        System.out.println("______________");
+
+        test.changeNorth();
+        System.out.println(test);
+        System.out.println("______________");
+
+        test.changeSouth();
+        System.out.println(test);
+        System.out.println("______________");
+
+        test.changeEast();
+        System.out.println(test);
+        System.out.println("______________");
+
+        test.changeWest();
+        System.out.println(test);
+        System.out.println("______________");
+
+        test.changeSouth();
+        System.out.println(test);
+        System.out.println("______________");
+
+        test.changeNorth();
+        System.out.println(test);
+        System.out.println("______________");
+    }
+
+}
---
+++ v2/Maze.java
@@ -0,0 +1,70 @@
+public class Maze{
+    /*
+    Contains Maze
+    Contains links to adjacent Mazes
+    */
+    
+    String[][] _maze;
+
+    private Maze northMaze;
+    private Maze eastMaze;
+    private Maze southMaze;
+    private Maze westMaze;
+
+
+
+    public Maze(String[][] arr){
+        this._maze = arr;
+    }
+
+    public void setNorth(Maze north){
+        this.northMaze = north;
+    }
+
+    public void setEast(Maze east){
+        this.eastMaze = east;
+    }
+
+    public void setSouth(Maze south){
+        this.southMaze = south;
+    }
+
+    public void setWest(Maze west){
+        this.westMaze = west;
+    }
+
+    public Maze getNorth(){
+        return northMaze;
+    }
+    public Maze getEast(){
+        return eastMaze;
+    }
+    public Maze getSouth(){
+        return southMaze;
+    }
+    public Maze getWest(){
+        return westMaze;
+    }
+
+    public void setPos(int x, int y, String cargo){
+        _maze[x][y] = cargo;
+    }
+
+    public String getPos(int x, int y){
+        return _maze[x][y];
+    }
+
+
+    public String toString(){
+        // Stringifies the maze
+        String retVal = "";
+        for(int i = 0; i < _maze.length; i++){
+            for(int e = 0; e < _maze[0].length; e++){
+                retVal += _maze[i][e];
+            }
+            retVal += "\n";
+        }
+        return retVal;
+    }
+
+}
