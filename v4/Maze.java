a5479fb4f96a1cbf48d93f34bf097a0601db8f79
Nakib Abedin <nabedin30@stuy.edu>
2022-05-27 13:20:00 -0400

    edit toString()

--- v4/Maze.java
+++ v4/Maze.java
@@ -60,7 +60,11 @@ public class Maze{
         String retVal = "";
         for(int i = 0; i < _maze.length; i++){
             for(int e = 0; e < _maze[0].length; e++){
-                retVal += _maze[i][e];
+                if(_maze[i][e].equals("#"))
+                   retVal = retVal + "\u001b[42m" + "\u001b[32m" + " " + "\u001b[30m" + "\u001b[40m" ;
+               else{
+                   retVal += _maze[i][e];
+            }
             }
             retVal += "\n";
         }
