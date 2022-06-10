c95023eb44e5b5d3b42073eee432c8037a4a9e20
nakibabedin <nabedin30@stuy.edu>
2022-06-09 20:30:04 -0400

    make stuff public

--- v10/Woo/TerminallyIll.java
+++ v10/Woo/TerminallyIll.java
@@ -6,23 +6,23 @@
 
 public class TerminallyIll
 {
-  private static final int BRIGHT = 1;
-  private static final int DARK = 2;
-  private static final int ITALICS = 3;
-  private static final int BLACK = 30;
-  private static final int RED = 31;
-  private static final int GREEN = 32;
-  private static final int YELLOW = 33;
-  private static final int BLUE = 34;
-  private static final int MAGENTA = 35;
-  private static final int CYAN = 36;
-  private static final int WHITE = 37;
-  private static final String CLEAR_SCREEN =  "\033[2J";
-  private static final String HIDE_CURSOR =  "\033[?25l";
-  private static final String SHOW_CURSOR =  "\033[?25h";
+  public static final int BRIGHT = 1;
+  public static final int DARK = 2;
+  public static final int ITALICS = 3;
+  public static final int BLACK = 30;
+  public static final int RED = 31;
+  public static final int GREEN = 32;
+  public static final int YELLOW = 33;
+  public static final int BLUE = 34;
+  public static final int MAGENTA = 35;
+  public static final int CYAN = 36;
+  public static final int WHITE = 37;
+  public static final String CLEAR_SCREEN =  "\033[2J";
+  public static final String HIDE_CURSOR =  "\033[?25l";
+  public static final String SHOW_CURSOR =  "\033[?25h";
 
   //use this to go back to normal terminal colors
-  private static final String RESET = color(40,37)+SHOW_CURSOR;
+  public static final String RESET = color(40,37)+SHOW_CURSOR;
 
   //use this to convert from color to background (30 to 37 becomes 40 to 47)
   public static int background(int color)
@@ -32,28 +32,28 @@ public class TerminallyIll
 
   //terminal specific character to move the cursor to a location
   //top left is 1,1
-  private static String go(int x, int y)
+  public static String go(int x, int y)
   {
     return ("\033[" + x + ";" + y + "H");
   }
 
     
-  private static String color(int a, int b)
+  public static String color(int a, int b)
   {
     return ("\033[0;" + a+ ";" + b + "m");
   }
-  private static String color(int a, int b, int c)
+  public static String color(int a, int b, int c)
   {
     return ("\033[0;" + a+ ";" + b + ";" + c+ "m");
   }
-  private static String color(int a, int b, int c, int d)
+  public static String color(int a, int b, int c, int d)
   {
     return ("\033[0;" + a+ ";" + b + ";" + c + ";" + d + "m");
   }
 
 
   //delay printing...
-  private static void wait(int millis)
+  public static void wait(int millis)
   {
     try {
       Thread.sleep(millis);
